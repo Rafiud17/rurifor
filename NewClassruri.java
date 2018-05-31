a83f80d58e633eeaaa4f87942583cc6d15548f42
Rafiud17 <39791692+Rafiud17@users.noreply.github.com>
2018-05-31 15:38:12 +0800

    Add files via upload

---
+++ NewClassruri.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hp
+ */
+public class NewClassruri {
+    public static void main(String[] args) {
+        for(int i=1; i<11; i++)
+            System.out.println(i+" * 8 = "+(i*8));
+    }
+}
